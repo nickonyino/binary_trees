528cd08ecdc65938ec1c44f5939d545762597d82
nickonyino <bahatiluiz@gmail.com>
2024-02-29 11:25:36 -0800

    tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,3 +1,5 @@
+#include "binary_trees.h"
+
 /**
  * binary_tree_preorder - Goes through a binary tree using pre-order traversal.
  * @tree: A pointer to the root node of the tree to traverse.
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - Goes through a binary tree using postorder traversal.
- * @tree: A pointer to the root node of the tree to traverse.
- * @func: A pointer to a function to call for each node.
+ * binary_tree_postorder - Traverses a binary tree with the post-order traversal.
+ * @tree: A pointer to root node of the tree to traverse.
+ * @func: A pointer to function to call for each node.
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
